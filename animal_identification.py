fb620b3d32c0b821661db7af131f313f6f9c7a44
GihanSVND <gihansvnd@gmail.com>
2024-05-30 10:57:37 +0530

    identification code part added

--- animal_identification.py
+++ animal_identification.py
@@ -1,5 +1,10 @@
 import functions_framework
 from PIL import Image
+import cv2
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow import keras
 
 # Triggered by a change in a storage bucket
 @functions_framework.cloud_event
@@ -16,8 +21,9 @@ def hello_gcs(cloud_event):
     updated = data["updated"]
 
     image_path = f"gs://{bucket}/{name}"  # Construct the full path
-
-    
+    model_path = f"gs://trained_classification_model/model.keras"
+    animal = detection(model_path,image_path)
+   
     print(f"Event ID: {event_id}")
     print(f"Event type: {event_type}")
     print(f"Bucket: {bucket}")
@@ -25,3 +31,17 @@ def hello_gcs(cloud_event):
     print(f"Metageneration: {metageneration}")
     print(f"Created: {timeCreated}")
     print(f"Updated: {updated}")
+    print(animal)
+
+
+def detection(mod_add,img_add):
+    class_names = ['elephant', 'none', 'peacock', 'wildboar']
+    model = keras.models.load_model(mod_add)
+    image = cv2.imread(img_add)
+    IMAGE_SIZE = (128,128)
+    resized_image = tf.image.resize(image,IMAGE_SIZE)
+    scaled_image = resized_image/255
+    predictions = model.predict(np.expand_dims(scaled_image, axis=0))
+    predicted_class_index = np.argmax(predictions)
+    detected_animal = class_names[predicted_class_index]
+    return detected_animal
